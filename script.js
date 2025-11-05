7f37a445664466578801d11f7fd36b6e51bcc887
igiblack <95640789+SeN1237@users.noreply.github.com>
2025-11-05 23:50:06 +0100

    Remove unused Firestore imports from script.js

--- script.js
+++ script.js
@@ -1,4 +1,3 @@
-import { getFirestore, collection, addDoc, getDocs, updateDoc } from "firebase/firestore";
 // --- SEKCJA 0: IMPORTY I KONFIGURACJA FIREBASE ---
 import { initializeApp } from "https://www.gstatic.com/firebasejs/12.5.0/firebase-app.js";
 import { 
